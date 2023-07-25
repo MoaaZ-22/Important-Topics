d61133528e3cd57755e13a3322d37ddcd1aa8775
MoaaZ-22 <moazmuhammed78@gmail.com>
2023-07-25 11:48:47 +0300

    Added comments to connectivity cubit

--- lib/connectivity/cubits/connectivity_cubit.dart
+++ lib/connectivity/cubits/connectivity_cubit.dart
@@ -6,36 +6,54 @@ import 'package:meta/meta.dart';
 
 part 'connectivity_state.dart';
 
+// Step 1: Define an enumeration for connectivity status to have clear states.
 enum ConnectivityStatus { connected, disconnected }
 
 class ConnectivityCubit extends Cubit<ConnectivityState> {
+  // Step 2: Initialize the cubit with the initial state (ConnectivityInitial).
   ConnectivityCubit() : super(ConnectivityInitial());
 
   final Connectivity connectivity = Connectivity();
   StreamSubscription? connectivityStreamSubscription;
+
+  // Step 3: Add a variable to store the last known connectivity status.
   ConnectivityStatus lastConnectivityStatus = ConnectivityStatus.connected;
 
+  // Step 4: This method will be used to start listening to connectivity changes.
   void checkConnectivity() {
+    // Cancel the existing stream subscription if it exists to avoid multiple listeners.
     connectivityStreamSubscription?.cancel();
+
+    // Start listening to the onConnectivityChanged stream provided by the Connectivity plugin.
     connectivityStreamSubscription =
         connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
-          final isConnected = (result == ConnectivityResult.wifi ||
-              result == ConnectivityResult.mobile);
+      // Check if the device is connected to either Wi-Fi or mobile data.
+      final isConnected = (result == ConnectivityResult.wifi ||
+          result == ConnectivityResult.mobile);
 
-          if (isConnected && lastConnectivityStatus == ConnectivityStatus.disconnected) {
-            emit(ConnectivityHasBeenRestoredState());
-          } else if (!isConnected && lastConnectivityStatus == ConnectivityStatus.connected) {
-            emit(ConnectivityLostConnectionState());
-          }
+      // Step 5: Emit a state based on connectivity changes.
+      // If the device is connected and was previously disconnected, emit ConnectivityHasBeenRestoredState.
+      if (isConnected &&
+          lastConnectivityStatus == ConnectivityStatus.disconnected) {
+        emit(ConnectivityHasBeenRestoredState());
+      }
+      // If the device is disconnected and was previously connected, emit ConnectivityLostConnectionState.
+      else if (!isConnected &&
+          lastConnectivityStatus == ConnectivityStatus.connected) {
+        emit(ConnectivityLostConnectionState());
+      }
 
-          lastConnectivityStatus = isConnected
-              ? ConnectivityStatus.connected
-              : ConnectivityStatus.disconnected;
-        });
+      // Update the last known connectivity status for the next comparison.
+      lastConnectivityStatus = isConnected
+          ? ConnectivityStatus.connected
+          : ConnectivityStatus.disconnected;
+    });
   }
 
   @override
+  // Step 6: Override the close method to clean up resources when the cubit is closed.
   Future<void> close() {
+    // Cancel the stream subscription to avoid memory leaks.
     connectivityStreamSubscription?.cancel();
     return super.close();
   }
